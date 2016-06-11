7c8090e63e4604915dac44558b7380559a2018c8
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2016-06-12 00:48:07 +0800

    index layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <br>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-4">
+                <a href="#">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">第一組</div>
+
+                    <div class="panel-body">
+                        <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                    </div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="#">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">第二組</div>
+
+                    <div class="panel-body">
+                        <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                    </div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="#">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">第三組</div>
+
+                    <div class="panel-body">
+                        <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-4">
+                <a href="#">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">第四組</div>
+
+                        <div class="panel-body">
+                            <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="#">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">第五組</div>
+
+                    <div class="panel-body">
+                        <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                    </div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="#">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">第六組</div>
+
+                    <div class="panel-body">
+                        <img class="img-rounded img-responsive" src="/img/Title.JPG" alt="test">
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
